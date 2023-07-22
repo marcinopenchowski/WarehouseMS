08480fb255d7d4d593dc66d5da3a0358090b5a7a
Filip <47536273+tfkk@users.noreply.github.com>
2023-07-22 21:33:05 +0200

    fixed card design and added popup on button click

--- frontend/wms/src/routes/profile/Profile.jsx
+++ frontend/wms/src/routes/profile/Profile.jsx
@@ -4,12 +4,16 @@ import { Header, Navbar, ProfileCard } from '../../components'
 export default function Profile() {
   return (
     <div className="flex h-screen">
-      <div>
+      <div className="w-fit">
         <Navbar />
       </div>
-      <div className="flex-1">
-        <Header />
-        <ProfileCard />
+      <div className="flex-col w-full h-screen">
+        <div>
+          <Header />
+        </div>
+        <div className='flex items-center justify-center'>
+          <ProfileCard />
+        </div>
       </div>
     </div>
   )
